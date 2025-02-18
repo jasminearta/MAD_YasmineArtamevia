0373e4668a75852847067542ef2c0875ad481062
Jasmine <user@Jasmines-MacBook.local>
2025-02-18 20:00:42 +0700

    Tugas 1

---
+++ minggu1/tugas1.dart
@@ -0,0 +1,12 @@
+void main() {
+  String namaProduk = "Laptop";
+  int hargaSatuan = 80000000;
+  int jumlahBeli = 2;
+
+  int totalHarga = hargaSatuan * jumlahBeli;
+
+  print("Nama Produk  : $namaProduk");
+  print("Harga Satuan : Rp$hargaSatuan");
+  print("Jumlah Beli  : $jumlahBeli");
+  print("Total Harga  : Rp$totalHarga");
+}
